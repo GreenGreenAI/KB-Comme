20782337db6b5c99d43d247c4c0bd87e0be4ebf2
kyne0127 <benedict0127@naver.com>
2026-07-28 13:55:05 +0900

    feat: bind compliance gates to user declarations

--- src/tradeflow/knowledge/__init__.py
+++ src/tradeflow/knowledge/__init__.py
@@ -1,5 +1,12 @@
 """Versioned source, rule and evidence layer."""
 
+from tradeflow.knowledge.compliance_declarations import (
+    DECLARABLE_GATEWAY_FIELDS,
+    USER_DECLARATION_SOURCE_ID,
+    ComplianceDeclarationAssembler,
+    ComplianceDeclarationInput,
+    ComplianceGatewayDeclaration,
+)
 from tradeflow.knowledge.evidence import validate_evidence_contract
 from tradeflow.knowledge.eligibility_evidence import (
     CompanyQualificationEvidence,
@@ -19,7 +26,11 @@ from tradeflow.knowledge.repository import KnowledgeRepository
 
 __all__ = [
     "KnowledgeRepository",
+    "ComplianceDeclarationAssembler",
+    "ComplianceDeclarationInput",
+    "ComplianceGatewayDeclaration",
     "CompanyQualificationEvidence",
+    "DECLARABLE_GATEWAY_FIELDS",
     "EligibilityEvidenceAssembler",
     "EligibilityEvidenceProvider",
     "EligibilityFactInput",
@@ -28,6 +39,7 @@ __all__ = [
     "KsureCaseProfile",
     "KsureCreditEvidence",
     "MutualAccountTimeline",
+    "USER_DECLARATION_SOURCE_ID",
     "derive_mutual_account_timeline",
     "validate_evidence_contract",
 ]
